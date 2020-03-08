fc21917a188d8edc6ec7ea5931c2a8072cdc5f51
Damon <dmarcr1997@gmail.com>
2020-03-08 14:58:30 -0500

    finished new messages and single message controls

--- app/controllers/MessageController.rb
+++ app/controllers/MessageController.rb
@@ -25,9 +25,11 @@ class MessageController < ApplicationController
             puts 'invalid message'
             redirect to '/messages/new'
         else    
-            if exists(params[:recipient])
+            if exists?(params[:recipient])
                 @recipient = Student.find_by(username: params[:recipient])
-                Message.create(message: params[:message], student_id: current_student, recipient: @recipient.id)
+                Message.create(message: params[:message], student_id: current_student.id, recipient: @recipient.id)
+                current_student.save
+                redirect to '/messages'
             else
                 puts 'no user by that name'
                 redirect to
@@ -39,10 +41,16 @@ class MessageController < ApplicationController
     get '/messages/:id' do
         if logged_in?
             @message = Message.find_by(id: params[:id])
-            if current_student.id == @message.student_id || current_student.id == @message.recipient
+            if current_student.id == @message.student_id 
+                @student = current_student
+                @recipient = Student.find_by(id: @message.recipient)
                 erb :'message/show'
+            elsif current_student.id == @message.recipient
+                @sender = Student.find_by(id: @message.student_id)
+                erb :'message/show'   
             else
                 puts 'message does not belong to you'
+                redirect to '/messages'
             end
         else
             puts 'must be logged in to view messages'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,34 @@
 require_relative '../config/environment'
 
-# Student.create(username: "Joe", password: "myNameisJoe", email:'jojo18@email.com')
+Student.create(username: "Joe", password: "myNameisJoe", email:'jojo18@email.com')
 
-# Student.create(username: "Melissa", password: "myNameisMelissa", email:'melissa18@email.com')
+Student.create(username: "Melissa", password: "myNameisMelissa", email:'melissa18@email.com')
 
-# Student.create(username: "Denise", password: "myNameisDenise", email:'denise18@email.com')
+Student.create(username: "Denise", password: "myNameisDenise", email:'denise18@email.com')
 
-# Student.create(username: "Patrick", password: "myNameisPat", email:'Patrick18@email.com')
+Student.create(username: "Patrick", password: "myNameisPat", email:'Patrick18@email.com')
 
-# Assignment.create(name: "calc ch2 derivatives", class_name: "Calculus", due_date: "01/12/20", student_id: 1, stucla_id: 1)
+Assignment.create(name: "calc ch2 derivatives", class_name: "Calculus", due_date: "01/12/20", student_id: 1, stucla_id: 1)
 
-# Assignment.create(name: "History Columbus", class_name: "History", due_date: "02/12/20", student_id: 1, stucla_id: 2)
+Assignment.create(name: "History Columbus", class_name: "History", due_date: "02/12/20", student_id: 1, stucla_id: 2)
 
-# Assignment.create(name: "Hamlet overview", class_name: "English", due_date: "01/24/20", student_id: 1, stucla_id: 3)
+Assignment.create(name: "Hamlet overview", class_name: "English", due_date: "01/24/20", student_id: 1, stucla_id: 3)
 
-# Assignment.create(name: 'Intro to Cells', class_name: 'Biology', due_date: "01/30/20", student_id: 1, stucla_id: 4)
+Assignment.create(name: 'Intro to Cells', class_name: 'Biology', due_date: "01/30/20", student_id: 1, stucla_id: 4)
 
-# Assignment.create(name: "History Columbus", class_name: "History", due_date: "02/12/20", student_id: 2, stucla_id: 2)
+Assignment.create(name: "History Columbus", class_name: "History", due_date: "02/12/20", student_id: 2, stucla_id: 2)
 
-# Assignment.create(name: "Hamlet overview", class_name: "English", due_date: "01/24/20", student_id: 3, stucla_id: 3)
+Assignment.create(name: "Hamlet overview", class_name: "English", due_date: "01/24/20", student_id: 3, stucla_id: 3)
 
-# Assignment.create(name: 'Intro to Cells', class_name: 'Biology', due_date: "01/30/20", student_id: 4, stucla_id: 4)
+Assignment.create(name: 'Intro to Cells', class_name: 'Biology', due_date: "01/30/20", student_id: 4, stucla_id: 4)
 
-# Stucla.create(name: 'Calculus')
+Stucla.create(name: 'Calculus')
 
-# Stucla.create(name: 'English')
+Stucla.create(name: 'English')
 
-# Stucla.create(name: 'History')
+Stucla.create(name: 'History')
 
-# Stucla.create(name: 'Biology')
+Stucla.create(name: 'Biology')
 
 Message.create(message: "Hi Melissa", student_id: 1, recipient: 2)
 
